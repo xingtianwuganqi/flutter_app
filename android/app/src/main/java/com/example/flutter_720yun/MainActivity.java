495fffbd4c7d04d39e559d6cfa4cd5ee9ce82b39
jingjun <254032134@qq.com>
2021-05-16 19:24:03 +0800

    fix：安卓配置

--- android/app/src/main/java/com/example/flutter_720yun/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_720yun/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.flutter_720yun;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+//import android.os.Bundle;
+//import io.flutter.app.FlutterActivity;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
+//
+//public class MainActivity extends FlutterActivity {
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    GeneratedPluginRegistrant.registerWith(this);
+//  }
+//}
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(FlutterEngine flutterEngine){
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
