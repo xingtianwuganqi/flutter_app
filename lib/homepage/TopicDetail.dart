28353f31ba850f8bf9a624129802026963fdda39
Ethan <rxswift@126.com>
2021-06-23 12:50:24 +0800

    标签调整

--- lib/homepage/TopicDetail.dart
+++ lib/homepage/TopicDetail.dart
@@ -112,7 +112,7 @@ class TopicDetailState extends State<TopicDetailWidget> {
               color: ColorsUtil.fromEnmu(ColorEnum.system),
               borderRadius: BorderRadius.all(Radius.circular(3.0))
           ),
-          padding: EdgeInsets.only(left: 5,right: 5,top: 1,bottom: 1),
+          padding: EdgeInsets.only(left: 5,right: 5,top: 3,bottom: 3),
           child: Text(e.tag_name ?? "",
             style: TextStyle(
               color: Colors.white,
