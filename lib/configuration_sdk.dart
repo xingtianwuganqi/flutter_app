7791a60831fc49f48c3433ae6a2fcb0a1b0b025b
Ethan <rxswift@126.com>
2023-03-28 11:15:03 +0800

    Update FindPetPage.dart

--- lib/configuration_sdk.dart
+++ lib/configuration_sdk.dart
@@ -1,23 +1,23 @@
-import 'dart:io';
+import 'package:platform_info/platform_info.dart';
 
 class Configuration {
-  static String appidStr = Platform.isIOS ? 'a5b0e8491845b3' : 'a641a7925cd0e3';
+  static String appidStr = Platform.I.buildMode == BuildMode.debug ? 'a62b013be01931' : 'a641a7925cd0e3';
 
-  static String appidkeyStr = Platform.isIOS
-      ? '7eae0567827cfe2b22874061763f30c9'
+  static String appidkeyStr = Platform.I.buildMode == BuildMode.debug
+      ? 'c3d0d2a9a9d451b07e62b509659f7c97'
       : '34957f626411ed7ac73916e8b4031128';
 
-  static String rewarderPlacementID = Platform.isIOS ? 'b5b72b21184aa8' : 'b5b449fb3d89d7';
-  static String interstitialPlacementID = Platform.isIOS ? 'b5bacad26a752a' : 'b5baca53984692';
-  static String bannerPlacementID = Platform.isIOS ? 'b5bacaccb61c29' : 'b641a834fd5243';
-  static String nativePlacementID = Platform.isIOS ? 'b5b0f5663c6e4a' : 'b5aa1fa2cae775';
+  static String rewarderPlacementID = Platform.I.buildMode == BuildMode.debug ? 'b5b72b21184aa8' : 'b5b449fb3d89d7';
+  static String interstitialPlacementID = Platform.I.buildMode == BuildMode.debug ? 'b5bacad26a752a' : 'b5baca53984692';
+  static String bannerPlacementID = Platform.I.buildMode == BuildMode.debug ? 'b62b03bacdcf28' : 'b641a834fd5243';
+  static String nativePlacementID = Platform.I.buildMode == BuildMode.debug ? 'b5b0f5663c6e4a' : 'b5aa1fa2cae775';
 
 
-  static String rewarderSceneID = Platform.isIOS ? 'f5e54970dc84e6' : '';
+  static String rewarderSceneID = Platform.I.buildMode == BuildMode.debug ? 'f5e54970dc84e6' : '';
 
-  static String nativeSceneID = Platform.isIOS ? 'f600938967feb5' : '';
+  static String nativeSceneID = Platform.I.buildMode == BuildMode.debug ? 'f600938967feb5' : '';
 
-  static String interstitialSceneID = Platform.isIOS ? 'f5e549727efc49' : '';
+  static String interstitialSceneID = Platform.I.buildMode == BuildMode.debug ? 'f5e549727efc49' : '';
 
-  static String bannerSceneID = Platform.isIOS ? 'f600938d045dd3' : '';
+  static String bannerSceneID = Platform.I.buildMode == BuildMode.debug ? 'f600938d045dd3' : '';
 }
