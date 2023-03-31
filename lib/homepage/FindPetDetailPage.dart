71d92f341fbe920fba54c311340f99ab67edc9be
Ethan <rxswift@126.com>
2023-03-31 13:06:58 +0800

    Update FindPetDetailPage.dart

--- lib/homepage/FindPetDetailPage.dart
+++ lib/homepage/FindPetDetailPage.dart
@@ -6,7 +6,7 @@ class FindPetDetailPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    throw UnimplementedError();
+    return FindPetDetailState();
   }
 }
 
@@ -34,16 +34,36 @@ class FindPetDetailState extends State<FindPetDetailPage> {
   Widget findPetHeader() {
     var header = Container(
       color: ColorsUtil.hexColor(0xF0EBDA),
+      padding: EdgeInsets.only(left: 15,right: 15),
       child: Row(
         children: [
-          Image.asset('assets/icons/icon_cat_header'),
-          Container(
-            margin: EdgeInsets.only(top: 15,left: 15,right: 15,bottom: 15),
-            color: ColorsUtil.hexColor(0xCBCFB5),
-            child: Container(
-              child: Padding(padding: EdgeInsets.only(left: 10,right: 10,top: 10,bottom: 10),
-                child: Text('没有找到想要的宠物？\n可以提交相关信息给小助手\n有合适的宠物后会通知您~'),),
+          GestureDetector(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: Image.asset('assets/icons/icon_cat_header.png',width: 50,height: 50,),
             ),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                // return NewUserInfoPage(pageType: MyPageType.otherPage,userId: data.userInfo.id);
+              }));
+            },
+          ),
+          Expanded(child:  
+            Container(
+              margin: EdgeInsets.only(top: 15,left: 15,right: 15,bottom: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(4)),
+                color: ColorsUtil.hexColor(0xCBCFB5),
+              ),
+              child: Container(
+                child: Padding(padding: EdgeInsets.only(left: 10,right: 10,top: 10,bottom: 10),
+                  child: Text('没有找到想要的宠物？\n可以提交相关信息给小助手\n有合适的宠物后会通知您~',
+                    style: TextStyle(fontSize: FontUtil.fs(FontSize.content),
+                        color: ColorsUtil.fromEnmu(ColorEnum.content),height: 1.3),
+                  ),
+                ),
+              ),
+            )
           )
         ],
       ),
